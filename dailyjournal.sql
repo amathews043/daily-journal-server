57921a2596078a9c1bc6aa36449f36437d938585
Annaliese Mathews <amesibov043@gmail.com>
2023-04-21 09:39:32 -0500

    started project. added database

---
+++ dailyjournal.sql
@@ -0,0 +1,36 @@
+CREATE TABLE Moods (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `label` TEXT NOT NULL
+);
+
+INSERT INTO Moods VALUES (null, 'Happy');
+
+
+INSERT INTO Moods VALUES (null, "Sad");
+INSERT INTO Moods VALUES (null, "Angry"); 
+INSERT INTO Moods VALUES (null, "Ok"); 
+
+
+SELECT * FROM Entries
+
+
+CREATE TABLE Entries (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `concept` TEXT NOT NULL,
+    `entry` TEXT NOT NULL,
+    `mood_id` TEXT NOT NULL, 
+    `date` TIMESTAMP NOT NULL,
+    FOREIGN KEY(`mood_id`) REFERENCES `Moods`(`id`)
+);
+
+
+INSERT INTO Entries VALUES (null, "JavaScript", 
+"I learned about loops today. They can be a lot of fun.\nI learned about loops today. They can be a lot of fun.\nI learned about loops today. They can be a lot of fun.",
+1, "Wed Sep 15 2021 10:10:47 ")
+
+
+INSERT INTO Entries VALUES (null, "Python", "Why did it take so long for python to have a switch statement? It's much cleaner than if/elif blocks", 
+3, "Wed Sep 15 2021 10:13:11 "); 
+INSERT INTO Entries VALUES (null, "JavaScript", "Dealing with Date is terrible. Why do you have to add an entire package just to format a date. It makes no sense.",
+3, "Wed Sep 15 2021 10:14:05 "); 
+
