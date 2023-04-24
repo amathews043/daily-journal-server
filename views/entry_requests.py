785e35b230f5615c052a1b6e3118bac4bdd0ccda
Annaliese Mathews <amesibov043@gmail.com>
2023-04-24 14:33:23 -0500

    added get_entry_by_search funtion

--- views/entry_requests.py
+++ views/entry_requests.py
@@ -1,5 +1,6 @@
 import sqlite3
 import json
+from datetime import datetime
 
 from models import Entries
 
@@ -63,3 +64,63 @@ def delete_entry(id):
         DELETE FROM Entries 
         WHERE id = ? 
         """, (id, ))
+
+def create_entry(entry):
+    with sqlite3.connect("./dailyjournal.sqlite3") as conn: 
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        INSERT INTO Entries
+            ( concept, entry, mood_id, date )
+        VALUES ( ?, ?, ?, ? )
+        """, (entry['concept'], entry['entry'], entry['mood_id'], entry['date'],))
+
+        id = db_cursor.lastrowid
+        entry['id'] = id 
+
+    return entry
+
+def update_entry(id, new_entry): 
+    with sqlite3.connect("./dailyjournal.sqlite3") as conn: 
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        UPDATE Entries 
+            SET 
+                concept = ?, 
+                entry = ?, 
+                mood_id = ?, 
+                date = ?
+        WHERE id = ?
+        """, (new_entry['concept'], new_entry['entry'], new_entry['mood_id'], new_entry['date'], id, ))
+    row_affected = db_cursor.rowcount
+
+    if row_affected == 0:
+        return False
+    else: 
+        return True
+    
+def get_entry_by_search(searchTerm): 
+    with sqlite3.connect("./dailyjournal.sqlite3") as conn: 
+
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT 
+            id, 
+            concept, 
+            entry, 
+            mood_id, 
+            date
+        FROM Entries
+        WHERE entry LIKE ? 
+        """,(f"%{searchTerm}%", ))
+
+        entries = []
+        dataset = db_cursor.fetchall()
+        for row in dataset: 
+            entry = Entries(row['id'], row['concept'], row['entry'], row['mood_id'], row['date'])
+            entries.append(entry.__dict__)
+            
+    return entries
